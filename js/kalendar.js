d4eecee527377fb48bf6e617ca8c2c0db5a8ed2b
cornx <exit>
2016-10-26 00:08:51 +0200

    ispravljen bug sa klikom

--- js/kalendar.js
+++ js/kalendar.js
@@ -1,22 +1,3 @@
-// označavanje dana onclick
-/*this.onclick = function() {
-    myFunction();
-};
-
-function myFunction() {
-    var dan = window.event.target;
-
-    if (event.shiftKey) {
-        dan.className = "prviDan";
-    } else if (event.ctrlKey) {
-        dan.className = "zadnjiDan";
-    } else if (event.altKey) {
-        dan.className = "";
-    } else {
-        dan.className = "zacrveni";
-    }
-}*/
-
 function tablica() {
     var dan = window.event.target;
 
@@ -29,9 +10,4 @@ function tablica() {
     } else {
         dan.className = "zacrveni";
     }
-}
-
-function sakrijUpute() {
-    document.querySelector(".upute").classList.toggle("sakrij");
-
 }

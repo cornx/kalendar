508ee1d2e9d35fc69994111d8538ecd6d74f41ae
cornx <exit>
2016-10-26 00:03:39 +0200

    ispravljen bug sa klikom

--- js/kalendar.js
+++ js/kalendar.js
@@ -1,26 +1,5 @@
-// registriraj varijable
-
-// pokupi vrijednost iz oba inputa
-
-/*function rezerviraj() {
-    var min = document.querySelector("#od"),
-        max = document.querySelector("#do"),
-        dan = document.getElementsByTagName("td");
-
-    for (var i = min.value - 1; i < max.value; i++) {
-        min.addClass = "prviDan";
-        max.addClass = "zadnjiDan";
-        var input = dan[i];
-        input.className += "zacrveni";
-        console.log(input.innerHTML)
-    }
-
-    console.log(min.value);
-    console.log(max.value);
-
-}*/
 // označavanje dana onclick
-this.onclick = function() {
+/*this.onclick = function() {
     myFunction();
 };
 
@@ -36,4 +15,23 @@ function myFunction() {
     } else {
         dan.className = "zacrveni";
     }
+}*/
+
+function tablica() {
+    var dan = window.event.target;
+
+    if (event.shiftKey) {
+        dan.className = "prviDan";
+    } else if (event.ctrlKey) {
+        dan.className = "zadnjiDan";
+    } else if (event.altKey) {
+        dan.className = "";
+    } else {
+        dan.className = "zacrveni";
+    }
+}
+
+function sakrijUpute() {
+    document.querySelector(".upute").classList.toggle("sakrij");
+
 }
